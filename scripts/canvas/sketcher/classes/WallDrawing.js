32c1a26dc3babe700ca56750ec7552e1e4c379a2
Brannon Dorsey <brannon@brannondorsey.com>
2013-12-02 17:37:54 -0600

    Fixed major drawing bug. Canvas is now drawing correctly.

--- scripts/canvas/sketcher/classes/WallDrawing.js
+++ scripts/canvas/sketcher/classes/WallDrawing.js
@@ -26,42 +26,12 @@ WallDrawing.prototype.saveImages = function(){
 }
 
 WallDrawing.prototype.updateImages = function(){
-
-	//borrowing method from:
-	//http://stackoverflow.com/questions/3309571/getdataurl-for-part-of-the-canvas-object
-	var index = this._getCurrentWallIndex();
-	// if(index == 0) index = 1;
-	// if(index == this.wallSegments.length - 1) index = this.wallSegments.length - 2;
-	console.log('the image number ' +  (index + 1) + ' is closest to the center');
-	var centerWallSegment = this.wallSegments[index];
-	var leftWallSegment = this.wallSegments[index - 1];
-	var rightWallSegment = this.wallSegments[index + 1];
-
-	var centerImageData = this.context.getImageData(centerWallSegment.x, 
-												    centerWallSegment.y, 
-												    centerWallSegment.size, 
-												    centerWallSegment.size);
-
-	var leftImageData = this.context.getImageData(leftWallSegment.x, 
-												  leftWallSegment.y, 
-												  leftWallSegment.size, 
-												  leftWallSegment.size);
-
-	var rightImageData = this.context.getImageData(rightWallSegment.x, 
-												   rightWallSegment.y, 
-												   rightWallSegment.size, 
-												   rightWallSegment.size);
-
-	//make a new temporary canvas to draw imageData object from the
-	//three wallSegments to and then save from
-	var tempCanvas = document.createElement('canvas');
-	var tempContext = tempCanvas.getContext('2d');
-	tempCanvas.width = centerWallSegment.size;
-	tempCanvas.height = centerWallSegment.size;
-
-	centerWallSegment.updateImage(tempCanvas, centerImageData);
-	leftWallSegment.updateImage(tempCanvas, leftImageData);
-	rightWallSegment.updateImage(tempCanvas, rightImageData);
+	
+	var visibleWalls = this._getVisibleWalls();
+	for(var i = 0; i < visibleWalls.length; i++){
+		var visibleWall = visibleWalls[i];
+		visibleWalls[i].updateImage();
+	}
 }
 
 //called onMouseUp if dragging tool was enabled. 
@@ -90,30 +60,20 @@ WallDrawing.prototype.display = function(){
 //------------------------------------------------------------------------
 //PROTECTED FUNCTIONS
 
-//returns the index of wall currently most in the frame of the canvas
-WallDrawing.prototype._getCurrentWallIndex = function(){
+//returns array of all wallSegements that are inside the canvas frame
+WallDrawing.prototype._getVisibleWalls = function(){
 		
-	var closestWallSegment = this.wallSegments[0];
+	var visibleWalls = [];
 	this._walkWallSegments(function(wallSegment){
-		
-		var frameCenterX = this.canvas.width/2;
-
-		var closestWallSegmentCenterX = closestWallSegment.x + closestWallSegment.size/2;
-		var currentClosestDist = Math.abs(frameCenterX - closestWallSegmentCenterX);
-
-		var wallSegmentCenterX = wallSegment.x + wallSegment.size/2;
-		var dist = Math.abs(frameCenterX - wallSegmentCenterX);
-
-		if(wallSegment.imageIndex == 3){
-			console.log('The distance between 3 and center is ' + dist);
-		}else if(wallSegment.imageIndex == 4){
-			console.log('The distance between 4 and center is ' + dist);
+		//if at least part of the wallSegment is in the canvas frame...
+		if(wallSegment.x > 0 &&
+		   wallSegment.x < this.canvas.width ||
+		   wallSegment.x + wallSegment.size > 0 &&
+		   wallSegment.x + wallSegment.size < this.canvas.width){
+			visibleWalls.push(wallSegment);
 		}
-		//if this wallSegment is closer to the center of the frame of the
-		//canvas replace closestWallSegment
-		if(dist < currentClosestDist) closestWallSegment = wallSegment;
 	});
-	return closestWallSegment.imageIndex - 1;
+	return visibleWalls;
 }
 
 WallDrawing.prototype._walkWallSegments = function(fn){
--- scripts/canvas/sketcher/classes/WallSegment.js
+++ scripts/canvas/sketcher/classes/WallSegment.js
@@ -23,19 +23,38 @@ WallSegment.prototype.display = function(){
 	}
 }
 
-WallSegment.prototype.updateImage = function(canvas, newImageData){
-
+WallSegment.prototype.updateImage = function(){
+	console.log(this.image);
 	var self = this;
 
-	var context = canvas.getContext('2d');
-	context.putImageData(newImageData, 0, 0);
-	var dataURL = canvas.toDataURL('image/png');
+	//create two memory-only canvas
+	var tempCanvas = this._createCanvas();
+	var tempContext = tempCanvas.getContext('2d');
+	var tempCanvas2 = this._createCanvas();
+	var tempContext2 = tempCanvas2.getContext('2d');
 
+	tempContext.drawImage(this.image, 0, 0); //draw the old image to the first mem-only canvas
+
+	//get the new image with all changes since the last drag (taken from the visible canvas)
+	var newImageData = this.context.getImageData(this.x, this.y, this.size, this.size);
+	tempContext2.putImageData(newImageData, 0, 0); //place the new image in the second mem-only canvas
+	var newImageURL = tempCanvas2.toDataURL('image/png');
 	var newImage = new Image();
-	newImage.src = dataURL;
+	newImage.src = newImageURL;
 	newImage.onload = function(){
-		self.image = newImage;
-		console.log(self.image)
+		
+		//draw the new image on top of the old image in the 
+		//first mem-only canvas
+		tempContext.drawImage(newImage, 0, 0);
+		var dataURL = tempCanvas.toDataURL('image/png');
+
+		//save the compilation as the new image
+		var combinedImage = new Image();
+		combinedImage.src = dataURL;
+		combinedImage.onload = function(){
+			self.image = combinedImage;
+			// console.log(self.image.src)
+		}
 	}
 }
 
@@ -43,6 +62,8 @@ WallSegment.prototype.loadImage = function(){
 	var self = this;
 	this.image = new Image();
 	this.image.src = this.imageURL;
+	this.image.height = this.size;
+	this.image.width = this.size;
 	this.image.onload = function() {
     	self._isLoaded = true;
 	}		  
@@ -60,5 +81,15 @@ WallSegment.prototype.inside = function(mouseX){
 	if(mouseX > this.x &&
 	   mouseX < this.x+this.size) return true;
 	else return false;
+}
+
+//------------------------------------------------------------------------
+//PROTECTED FUNCTIONS
+
+WallSegment.prototype._createCanvas = function(){
+	var tempCanvas = document.createElement('canvas');
+	tempCanvas.width = this.size;
+	tempCanvas.height = this.size;
+	return tempCanvas;
 }
 
