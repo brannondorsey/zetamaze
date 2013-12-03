877cbdfccf6e82afe84f4dab37b28851e1e7a482
Brannon Dorsey <brannon@brannondorsey.com>
2013-12-03 13:24:25 -0600

    Sketch canvas now loads images asynchronously when you use the drag to to navigate right and left. Also canvas now displays a random section of the wall on each page load.

--- scripts/canvas/sketcher/classes/WallDrawing.js
+++ scripts/canvas/sketcher/classes/WallDrawing.js
@@ -1,22 +1,29 @@
 function WallDrawing(canvas, numbImages){
 
 	//pick a random wallIndex to start on
-	//this.currentImageIndex = Math.ceil(Math.random()*numbImages-1);
-	this.currentImageIndex = 3;
+	this.currentImageIndex = Math.ceil(Math.random()*numbImages-1);
+	//this.currentImageIndex = 3;
 	this.wallSize = 512;
 	this.wallSegments = [];
 	this.canvas = canvas;
 	this.context = this.canvas.getContext('2d');
+	this.numbInitImagesToLoad = 10; //must be odd number
+	this._initImagesLoaded = false;
+	this.initWallSegments = [];
 
 	var startX = this.canvas.width/2 - this.currentImageIndex * this.wallSize;
 	var x = startX;
 
 	for(var i = 0; i < numbImages; i++){
 		var shouldLoad;
-		if(i < 10){
+		if(i > this.currentImageIndex - this.numbInitImagesToLoad/2 &&
+		   i < this.currentImageIndex + this.numbInitImagesToLoad/2){
 			shouldLoad = true;
 		}else shouldLoad = false;
 		this.wallSegments[i] = new WallSegment(this.context, x, 0, this.wallSize, i + 1, shouldLoad);
+		if(shouldLoad){
+			this.initWallSegments.push(this.wallSegments[i]);
+		}
 		x += this.wallSize;
 	}
 }
@@ -33,13 +40,43 @@ WallDrawing.prototype.updateImages = function(){
 }
 
 //called onMouseUp if dragging tool was enabled. 
-WallDrawing.prototype.loadNewImages = function(startX, endX){
+WallDrawing.prototype.loadImages = function(previousMouseX, mouseX){
+
+	//the number of images to keep loaded to each
+	//side of the frame
+	var imagesToBuffer = 4;
+	var visibleWalls = this._getVisibleWalls();
 
 	//images dragged right, load left
-	if(startX < endX){
+	if(previousMouseX < mouseX){
+		var leftVisibleWall = this._getLeftMostWall(visibleWalls);
+		var leftWallIndex = leftVisibleWall.imageIndex - 1;
 
-	}else{ //images dragged left, load right
+		//if we are not too close to the left most image
+		if(leftWallIndex >= imagesToBuffer){
+			for(var i = leftWallIndex; i > leftWallIndex - imagesToBuffer; i--){
+				var wallSegment = this.wallSegments[i];
+				if(!wallSegment.isLoaded()){
+					wallSegment.loadImage();
+					//console.log('I loaded an image to the left');
+				}
+			}
+		}
+		
+	}else if(previousMouseX > mouseX){ //images dragged left, load right
+		var rightVisibleWall = this._getRightMostWall(visibleWalls);
+		var rightWallIndex = rightVisibleWall.imageIndex - 1;
 
+		//if we are not too close to the right most image
+		if(rightWallIndex <= this.wallSegments.length - imagesToBuffer){
+			for(var i = rightWallIndex; i < rightWallIndex + imagesToBuffer; i++){
+				var wallSegment = this.wallSegments[i];
+				if(!wallSegment.isLoaded()){
+					wallSegment.loadImage();
+					//console.log('I loaded an image to the right');
+				}
+			}
+		}
 	}
 }
 
@@ -82,6 +119,24 @@ WallDrawing.prototype.notifyNeedsUpdate = function(previousMouseX, mouseX){
 	});
 }
 
+//returns true if initial images are loaded and checks 
+//again if they arent
+WallDrawing.prototype.initImagesLoaded = function(){
+	if(this._initImagesLoaded) return true;
+	else{
+		var numbLoaded = 0;
+		for(i = 0; i < this.initWallSegments.length; i++){
+			var initWallSegment = this.initWallSegments[i];
+			if(initWallSegment.isLoaded()) numbLoaded++;
+		}
+
+		//if all initial images have been loaded...
+		if(numbLoaded != 0 &&
+		   numbLoaded == this.initWallSegments.length) return true;
+		else return false;
+	}
+}
+
 //------------------------------------------------------------------------
 //PROTECTED FUNCTIONS
 
@@ -99,6 +154,24 @@ WallDrawing.prototype._getVisibleWalls = function(){
 		}
 	});
 	return visibleWalls;
+}
+
+WallDrawing.prototype._getLeftMostWall = function(wallSegmentsArray){
+	var leftMostWall = wallSegmentsArray[0];
+	for(var i = 0; i < wallSegmentsArray.length; i++){
+		var wallSegment = wallSegmentsArray[i];
+		if(Math.min(wallSegment.x, leftMostWall.x) == wallSegment.x) leftMostWall = wallSegment;
+	}
+	return leftMostWall;
+}
+
+WallDrawing.prototype._getRightMostWall = function(wallSegmentsArray){
+	var rightMostWall = wallSegmentsArray[0];
+	for(var i = 0; i < wallSegmentsArray.length; i++){
+		var wallSegment = wallSegmentsArray[i];
+		if(Math.max(wallSegment.x, rightMostWall.x) == wallSegment.x) rightMostWall = wallSegment;
+	}
+	return rightMostWall;
 }
 
 WallDrawing.prototype._walkWallSegments = function(fn){
