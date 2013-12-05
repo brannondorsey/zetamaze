20fd9ed47a3b1caa0fd9e6ef12f53a97678b028b
Brannon Dorsey <brannon@brannondorsey.com>
2013-12-05 12:24:42 -0600

    Changed non-mouse look controls to arrow keys

--- scripts/three/classes/CharacterController.js
+++ scripts/three/classes/CharacterController.js
@@ -2,6 +2,7 @@ function CharacterController(scene, camera){
 	
 	this.speed = 5;
 	this.lookSpeed = 100;
+	this.flyEnabled = true;
 
 	this.camera = camera;
 
@@ -33,14 +34,6 @@ CharacterController.prototype.update = function(delta){
 	var rotateAngle = Math.PI / 4 * delta;   // pi/4 radians (45 degrees) per second
 	var cursorSpeed = this.cursorSpeed * delta;	
 	
-	// if (keyboard.pressed("P"))
-	// {
-	// 	camera.position.set(0,35,10); // first-this.body view
-	// 	this.body.position.set(50,100,50);
-	// 	this.body.rotation.y = -Math.PI / 2.0;
-	// 	this.body.velocity = new THREE.Vector3(0,0,0);
-	// }
-	
 	// movement controls	
 	var move = { 
 		xDist: 0, 
@@ -49,52 +42,42 @@ CharacterController.prototype.update = function(delta){
 	};				
 	
 	// forwards/backwards
-	if (this.keyboard.pressed("W"))
-		move.zDist -= moveDistance;
-	if (this.keyboard.pressed("S"))
-		move.zDist += moveDistance;
-	// turn left/right
-	if (this.keyboard.pressed("Q"))
-		move.yAngle += rotateAngle;
-	if (this.keyboard.pressed("E"))
-		move.yAngle -= rotateAngle;
+	if (this.keyboard.pressed('W')) move.zDist -= moveDistance;
+	if (this.keyboard.pressed('S')) move.zDist += moveDistance;
 	// left/right (strafe)
-	if ( this.keyboard.pressed("A") )
-		move.xDist -= moveDistance;
-	if ( this.keyboard.pressed("D") )
-		move.xDist += moveDistance;
+	if ( this.keyboard.pressed('A') ) move.xDist -= moveDistance;
+	if ( this.keyboard.pressed('D') ) move.xDist += moveDistance;
+
+	// turn left/right
+	if (this.keyboard.pressed('left')) move.yAngle += rotateAngle;
+	if (this.keyboard.pressed('right')) move.yAngle -= rotateAngle;
+
+	// look up/down
+	if ( this.keyboard.pressed('up') ) this.camera.rotateX(  rotateAngle );
+	if ( this.keyboard.pressed('down') ) this.camera.rotateX( -rotateAngle );
 		
 	// process data from mouse look
 	//  (if inactive, there will be no change)
 	move.yAngle -= rotateAngle * this.mouseLook.x * 0.1;
 	this.mouseLook.x = 0;
-		
-	// up/down (debugging fly)
-	if ( this.keyboard.pressed("T") )
-	{
-		this.body.velocity = new THREE.Vector3(0,0,0);
-		this.body.translateY( moveDistance );
-	}
-	if ( this.keyboard.pressed("G") )
-	{
-		this.body.velocity = new THREE.Vector3(0,0,0);
-		this.body.translateY( -moveDistance );
-	}
+	
+	if(this.flyEnabled){
+		// up/down (debugging fly)
+		if ( this.keyboard.pressed('R') ){
+			this.body.velocity = new THREE.Vector3(0,0,0);
+			this.body.translateY( moveDistance );
+		}
+
+		if ( this.keyboard.pressed('F') ){
+			this.body.velocity = new THREE.Vector3(0,0,0);
+			this.body.translateY( -moveDistance );
+		}
+	}	
 	
 	this.body.translateZ( move.zDist );
 	this.body.rotateY( move.yAngle );
 	this.body.translateX( move.xDist );
 	this.body.updateMatrix();
-		
-	// look up/down
-	if ( this.keyboard.pressed("3") ) // third-p view
-		this.camera.position.set(0,50,250);
-	if ( this.keyboard.pressed("1") ) // first-p view
-		this.camera.position.set(0,35,10);
-	if ( this.keyboard.pressed("R") )
-		this.camera.rotateX(  rotateAngle );
-	if ( this.keyboard.pressed("F") )
-		this.camera.rotateX( -rotateAngle );
 		
 	// process data from mouse look
 	//  (if inactive, there will be no change)
@@ -104,7 +87,7 @@ CharacterController.prototype.update = function(delta){
 	// limit camera to +/- 45 degrees (0.7071 radians) or +/- 60 degrees (1.04 radians)
 	this.camera.rotation.x = THREE.Math.clamp( this.camera.rotation.x, -1.04, 1.04 );
 	// pressing both buttons moves look angle to horizon
-	if ( this.keyboard.pressed("R") && this.keyboard.pressed("F") )
+	if ( this.keyboard.pressed('R') && this.keyboard.pressed('F') )
 		this.camera.rotateX( -6 * this.camera.rotation.x * rotateAngle );
 	
 	// collision detection!
@@ -116,17 +99,10 @@ CharacterController.prototype.update = function(delta){
 	// 	this.body.updateMatrix();
 		
 	// 	if ( collision( walls ) )
-	// 		console.log( "Something's wrong with collision..." );
+	// 		console.log( 'Something's wrong with collision...' );
 		
 	// }
 	
-	// TODO: make sure there is no double-jump glitch
-	//	(e.g. hold down space sometimes results in double-jump)
-	// if ( keyboard.pressed("space") && (this.body.velocity.y == 0) )
-	// 	this.body.velocity = new THREE.Vector3(0,12,0);
-	
-	//this.body.velocity.add( gravity.clone().multiplyScalar( delta ) );
-	//this.body.translateY( this.body.velocity.y );
 	this.body.updateMatrix();
 }
 
