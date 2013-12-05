5c6b53a1d033d781dfd5d74bd359a67ccebba539
Brannon Dorsey <brannon@brannondorsey.com>
2013-12-05 13:14:04 -0600

    Working on updating the error handler to not allow islands of walls

--- scripts/canvas/maze/classes/ErrorHandler.js
+++ scripts/canvas/maze/classes/ErrorHandler.js
@@ -28,7 +28,8 @@ ErrorHandler.prototype.outputErrors = function(){
 ErrorHandler.prototype.checkErrors = function(maze, locations){
 	this.maze = maze;
 	this.locations = locations;
-	this.checkLocationValidaty(this.locations);
+	this.checkLocationvalidity(this.locations);
+	this.checkWallsConnected(this.maze);
 	if(this.errorsExist()) return true;
 	else{ //check for more errors
 		for(var key in this.locations){
@@ -40,12 +41,13 @@ ErrorHandler.prototype.checkErrors = function(maze, locations){
 										this.locations[key].mazeY);
 			if(!solver.isSolvable()) this.addError(key.capitalize() + " cannot be reached");	
 		}
+
 		if(this.errorsExist()) return true;
 	}
 	return false; //no errors were found
 }
 
-ErrorHandler.prototype.checkLocationValidaty = function(locations){
+ErrorHandler.prototype.checkLocationvalidity = function(locations){
 	for(var key in locations){
 		var location = locations[key];
 		if(this.maze[location.mazeY][location.mazeX] == 1){
@@ -54,7 +56,53 @@ ErrorHandler.prototype.checkLocationValidaty = function(locations){
 	}
 }
 
+ErrorHandler.prototype.checkWallsConnected = function(maze){
+
+	console.log('got in here');
+
+	var shouldBreak = false;
+	//don't check the outer rim
+	for(var y = 1; y < maze.length - 1; y++{
+		if(!shouldBreak){
+			for(var x = 1; x < maze[0].length - 1; x++){
+
+				//if this wall is on...
+				if(maze[y][x] == 1){
+
+					//check to make sure that one of the 
+					//8 ajacent walls is also on...
+					var up        = maze[y - 1][x];
+					var upRight   = maze[y - 1][x + 1];
+					var right     = maze[y][x + 1];
+					var rightDown = maze[y + 1][x + 1];
+					var down      = maze[y + 1][x];
+					var downLeft  = maze[y + 1][y - 1];
+					var left      = maze[y][x - 1];
+					var leftUp    = maze[y - 1][x - 1];
+
+					if(up == 1 ||
+					   upRight == 1 ||
+					   right == 1 ||
+					   rightDown == 1 ||
+					   down == 1 ||
+					   downLeft == 1 ||
+					   left == 1 ||
+					   leftUp == 1){
+						continue;
+					}else{
+						this.addError('All walls must be connected');
+						shouldBreak = true;
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
 ErrorHandler.prototype.errorsExist = function(){
 	return (this.errors.length > 0) ? true : false;
 }
+
+
 
