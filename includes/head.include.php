a314f90cdc6f1a86a604736b5027799bfa54b982
Brannon Dorsey <brannon@brannondorsey.com>
2013-12-08 21:24:52 -0600

    Started turning project into website

---
+++ includes/head.include.php
@@ -0,0 +1,1 @@
+<title>Zetamaze</title>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php require_once("includes/head.include.php"); ?>
+		<link rel="stylesheet" type="text/css" href="styles/index.css">
+	</head>
+
+	<body>
+		<div id="links">
+			<a href="play.php" id="play">play</a>
+			<a href="make.php" id="make">make</a>
+		</div>
+		<div id="description">
+			<p><strong>Zeta</strong>, a collaborative maze game by <a href="http://twitter.com/brannondorsey">@brannondorsey</a></p>
+		</div>
+	</body>
+</html>
