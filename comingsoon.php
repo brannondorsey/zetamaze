14f7d8da20dc6f54a36c3dde3a4b656c4f139320
Brannon Dorsey <brannon@brannondorsey.com>
2013-12-15 16:01:48 -0600

    Added coming soon page. Added liscense. Changed the order of navbar links to match the sites new standard.

---
+++ comingsoon.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php require_once("includes/head.include.php"); ?>
+		<link rel="stylesheet" type="text/css" href="styles/index.css">
+		<style>
+			div#links{
+				margin-top: 135px;
+				padding-bottom: 15px;
+				font-size: 48pt;
+
+			}
+
+			div.preview-images{
+				width: 680px;
+				margin: 40px auto;
+			}
+
+			div.preview-images img{
+				width: 200px;
+				height: 200px;
+				margin: 0px 10px;
+			}
+		</style>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-46480710-1', 'zetamaze.com');
+		  ga('send', 'pageview');
+
+		</script>
+	</head>
+
+	<body>
+		<div id="links">
+			Coming January 30th, 2014
+		</div>
+
+		<div class="preview-images">
+			<img src="images/make_preview.jpg" />
+			<img src="images/draw_preview.jpg" />
+			<img src="images/play_preview.jpg" />
+		</div>
+		<div id="description">
+			<p>
+ 			Zeta is a collaborative web game that allows visitors to edit, 
+ 			draw on, and explore a digital 3D maze. The project lives and 
+ 			grows on the web driven by the creative actions and decisions 
+ 			of those who participate.
+      		</p>
+			<p style="text-align: center">
+				<a href="http://twitter.com/brannondorsey">@brannondorsey</a> 
+				| code on <a href="http://github.com/brannondorsey/zetamaze">GitHub</a>
+			</p>
+		</div>
+	</body>
+</html>
--- includes/navbar.include.php
+++ includes/navbar.include.php
@@ -2,9 +2,9 @@
 	<div class="content">
 		<a href="index.php"><h1>Zeta <span>beta</span></h1></a>
 		<div id="links">
-			<a href="play.php">play</a> / 
 			<a href="make.php">make</a> / 
-			<a href="draw.php">draw</a>
+			<a href="draw.php">draw</a> /
+			<a href="play.php">play</a>
 		</div>
 	</div>
 </div>
